904b78f23daa61d25373f2a0ad1dadb9a224c2aa
tanqueta88 <oliverprim783@gmail.com>
2024-07-05 17:21:58 -0300

    The general structure of the project is modified

--- LoginNationalityService/src/app/components/login/login.component.ts
+++ LoginNationalityService/src/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import { UsersService } from '../../services/users.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   email: string = '';
