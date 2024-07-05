ab34b3bfbe83ff70a4ade5722e4a8cb083c9c6b5
tanqueta88 <oliverprim783@gmail.com>
2024-07-05 16:08:06 -0300

    Modifications in login component

--- LoginNationalityService/src/app/components/login/login.component.ts
+++ LoginNationalityService/src/app/components/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = '';
+  lat: string = '';
+  showError: boolean = false;
+  errorMessage: string = '';
 
+  constructor(private usersService: UsersService, private router: Router) {}
+
+  onLogin() {
+    this.usersService
+      .login(this.email, this.lat)
+      .subscribe((isValid: boolean) => {
+        if (isValid) {
+          this.router.navigate(['/main']);
+        } else {
+          this.errorMessage = 'Datos incorrectos. Revise usuario y/o clave';
+          this.showError = true;
+        }
+      });
+  }
 }
