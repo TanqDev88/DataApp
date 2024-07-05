28ec0d2eadd3c6f8a89345e10923842b8b477790
tanqueta88 <oliverprim783@gmail.com>
2024-07-05 16:09:00 -0300

    Modifications in users service

--- LoginNationalityService/src/app/services/users.service.ts
+++ LoginNationalityService/src/app/services/users.service.ts
@@ -1,9 +1,25 @@
+import { User } from './../models/User';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private urlApi = "https://jsonplaceholder.typicode.com/users";
+
+  login(email: string, lat: string){
+    return this.http.get<User[]>(this.urlApi)
+    .pipe(
+      map((users: User[]) => {
+        const user = users.find(u => u.email === email && u.address.geo.lat === lat);
+        return user ? true : false;
+      })
+    );
+  }
 }
