651855ce6f24de50ce658c24295ab6e48116fe5a
tanqueta88 <oliverprim783@gmail.com>
2024-07-06 02:58:31 -0300

    Modifications in data service

--- LoginNationalityService/src/app/services/data.service.ts
+++ LoginNationalityService/src/app/services/data.service.ts
@@ -1,9 +1,26 @@
+import { Gender } from '../models/Gender';
+import { Nationalize } from '../models/Nationality';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class InformationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private genderUrlApi = "https://api.genderize.io/?name=";
+  private nationalizeUrlApi = "https://api.nationalize.io/?name=";
+
+  determineGender(name: string) {
+    return this.http.get<Gender>(this.genderUrlApi + name);
+  }
+
+  determineNationality(name: string) {
+    return this.http.get<Nationalize>(this.nationalizeUrlApi + name);
+  }
+
 }
+
