d83fa8e343fdc2a2a795b853eef35698190a1b3a
tanqueta88 <oliverprim783@gmail.com>
2024-07-06 03:02:42 -0300

    Modifications in app-routing module

--- LoginNationalityService/src/app/app-routing.module.ts
+++ LoginNationalityService/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  {path: "main", component: MainSiteComponent},
+  {path: "login", component: LoginComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
