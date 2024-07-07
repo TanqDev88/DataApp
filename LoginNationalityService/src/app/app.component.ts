ff8197cb1e24d622fd4957bd0c613c8ac6c8f781
tanqueta88 <oliverprim783@gmail.com>
2024-07-07 19:11:45 -0300

    Modifications in app.component

--- LoginNationalityService/src/app/app.component.ts
+++ LoginNationalityService/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'LoginNationalityService';
+
+  constructor(private translate: TranslateService) {
+    const browserLang = translate.getBrowserLang();
+    if (browserLang) {
+      translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
+    } else {
+      translate.use('en');
+    }
+  }
 }
