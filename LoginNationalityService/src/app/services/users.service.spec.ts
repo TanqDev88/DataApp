91e3834e0cb617a045cdffd9cffa0961bb82f60c
tanqueta88 <oliverprim783@gmail.com>
2024-07-08 15:17:00 -0300

    Unit test are implemented for the users service

--- LoginNationalityService/src/app/services/users.service.spec.ts
+++ LoginNationalityService/src/app/services/users.service.spec.ts
@@ -1,16 +1,101 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { UsersService } from './users.service';
+import { User } from './../models/User';
 
-// import { UsersService } from './users.service';
+describe('UsersService', () => {
+  let service: UsersService;
+  let httpMock: HttpTestingController;
 
-// describe('UsersService', () => {
-//   let service: UsersService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
+    service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(UsersService);
-//   });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return true for correct login credentials', (done: DoneFn) => {
+    const mockUsers: User[] = [
+      {
+        id: 1,
+        name: 'John Doe',
+        username: 'johndoe',
+        email: 'test@example.com',
+        address: {
+          street: 'Kulas Light',
+          suite: 'Apt. 556',
+          city: 'Gwenborough',
+          zipcode: '92998-3874',
+          geo: {
+            lat: '37.3159',
+            lng: '81.1496'
+          }
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+          name: 'Romaguera-Crona',
+          catchPhrase: 'Multi-layered client-server neural-net',
+          bs: 'harness real-time e-markets'
+        }
+      }
+    ];
+
+    service.login('test@example.com', '37.3159').subscribe((isValid) => {
+      expect(isValid).toBe(true);
+      done();
+    });
+
+    const req = httpMock.expectOne(service['urlApi']);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
+  });
+
+  it('should return false for incorrect login credentials', (done: DoneFn) => {
+    const mockUsers: User[] = [
+      {
+        id: 1,
+        name: 'John Doe',
+        username: 'johndoe',
+        email: 'test@example.com',
+        address: {
+          street: 'Kulas Light',
+          suite: 'Apt. 556',
+          city: 'Gwenborough',
+          zipcode: '92998-3874',
+          geo: {
+            lat: '37.3159',
+            lng: '81.1496'
+          }
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+          name: 'Romaguera-Crona',
+          catchPhrase: 'Multi-layered client-server neural-net',
+          bs: 'harness real-time e-markets'
+        }
+      }
+    ];
+
+    service.login('test@example.com', 'wronglat').subscribe((isValid) => {
+      expect(isValid).toBe(false);
+      done();
+    });
+
+    const req = httpMock.expectOne(service['urlApi']);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
+  });
+});
+
