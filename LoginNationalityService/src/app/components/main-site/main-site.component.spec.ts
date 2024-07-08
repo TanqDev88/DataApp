169b841c2b1508ef9cd51b04aa3f582e35b46b2f
tanqueta88 <oliverprim783@gmail.com>
2024-07-08 15:04:52 -0300

    Unit test are implemented for the main-site component

--- LoginNationalityService/src/app/components/main-site/main-site.component.spec.ts
+++ LoginNationalityService/src/app/components/main-site/main-site.component.spec.ts
@@ -1,23 +1,102 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { MainSiteComponent } from './main-site.component';
+import { InformationService } from '../../services/data.service';
+import { Gender } from '../../models/Gender';
+import { Nationalize } from '../../models/Nationality';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-// import { MainSiteComponent } from './main-site.component';
+// Factory function for the TranslateHttpLoader
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
-// describe('MainSiteComponent', () => {
-//   let component: MainSiteComponent;
-//   let fixture: ComponentFixture<MainSiteComponent>;
+describe('MainSiteComponent', () => {
+  let component: MainSiteComponent;
+  let fixture: ComponentFixture<MainSiteComponent>;
+  let informationService: InformationService;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [MainSiteComponent]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MainSiteComponent],
+      imports: [
+        FormsModule,  // Add FormsModule here
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [InformationService]
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(MainSiteComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(MainSiteComponent);
+    component = fixture.componentInstance;
+    informationService = TestBed.inject(InformationService);
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call determineGender and set genderData on onSubmit', () => {
+    const mockGenderData: Gender = {
+      name: 'male', probability: 0.99, count: 1000,
+      gender: ''
+    };
+    spyOn(informationService, 'determineGender').and.returnValue(of(mockGenderData));
+
+    component.name = 'John';
+    component.onSubmit();
+
+    expect(informationService.determineGender).toHaveBeenCalledWith('John');
+    expect(component.genderData).toEqual(mockGenderData);
+  });
+
+  it('should call determineNationality and set nationalityData on onSubmit', () => {
+    const mockNationalityData: Nationalize = {
+      country: [{ country_id: 'US', probability: 0.5 }],
+      count: 0,
+      name: ''
+    };
+    spyOn(informationService, 'determineNationality').and.returnValue(of(mockNationalityData));
+
+    component.name = 'John';
+    component.onSubmit();
+
+    expect(informationService.determineNationality).toHaveBeenCalledWith('John');
+    expect(component.nationalityData).toEqual(mockNationalityData);
+  });
+
+  it('should call both determineGender and determineNationality on onSubmit', () => {
+    const mockGenderData: Gender = {
+      name: 'male', probability: 0.99, count: 1000,
+      gender: ''
+    };
+    const mockNationalityData: Nationalize = {
+      country: [{ country_id: 'US', probability: 0.5 }],
+      count: 0,
+      name: ''
+    };
+
+    spyOn(informationService, 'determineGender').and.returnValue(of(mockGenderData));
+    spyOn(informationService, 'determineNationality').and.returnValue(of(mockNationalityData));
+
+    component.name = 'John';
+    component.onSubmit();
+
+    expect(informationService.determineGender).toHaveBeenCalledWith('John');
+    expect(informationService.determineNationality).toHaveBeenCalledWith('John');
+    expect(component.genderData).toEqual(mockGenderData);
+    expect(component.nationalityData).toEqual(mockNationalityData);
+  });
+});
